3454ce7aa101346f43aa411f96a3046a1edf8071
Tristan Schmitt <tristanrschmitt@gmail.com>
2018-09-27 16:50:50 -0400

    Enable CORS (Cross-Origin Resource Sharing) on the server.

--- demo/src/main/java/com/example/demo/CoolCarController.java
+++ demo/src/main/java/com/example/demo/CoolCarController.java
@@ -2,8 +2,10 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import java.util.Collection;
 import java.util.stream.Collectors; 
+
 
 @RestController
 class CoolCarController {
@@ -14,6 +16,7 @@ class CoolCarController {
 	}
 	
 	@GetMapping("/cool-cars")
+	@CrossOrigin(origins = "http://localhost:4200")
 	public Collection<Car> coolCars() {
 		return repository.findAll().stream()
 				.filter(this::isCool)
