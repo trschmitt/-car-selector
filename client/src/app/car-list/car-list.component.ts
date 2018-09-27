33b237f2bc270909ad8200f32419ed97c5c1a4f7
Tristan Schmitt <tristanrschmitt@gmail.com>
2018-09-27 16:45:26 -0400

    Fixed /cannotget OnInit error

--- client/src/app/car-list/car-list.component.ts
+++ client/src/app/car-list/car-list.component.ts
@@ -1,4 +1,11 @@
+import { Component, OnInit } from '@angular/core';
 import { CarService } from '../shared/car/car.service';
+
+@Component({
+  selector: 'app-car-list',
+  templateUrl: './car-list.component.html',
+  styleUrls: ['./car-list.component.css']
+})
 
 export class CarListComponent implements OnInit {
   cars: Array<any>;
@@ -10,4 +17,5 @@ export class CarListComponent implements OnInit {
       this.cars = data;
     })
   }
+
 }
